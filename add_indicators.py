8d76164d3cbac6d89a5337e3f48047d22b2b6c4d
roshan-h-shah <rh.shah@ufl.edu>
2025-02-02 22:40:10 -0500

    Update add_indicators.py
    Added our indicators (DRI, VAS) and removed numpy

--- add_indicators.py
+++ add_indicators.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import ta
 
 def add_indicators(df):
@@ -20,7 +19,11 @@ def add_indicators(df):
     # Volume Indicators
     df['OBV'] = ta.volume.on_balance_volume(df['Close'], df['Volume'])
     df['CMF_20'] = ta.volume.chaikin_money_flow(df['High'], df['Low'], df['Close'], df['Volume'], window=20)
+    df['Vol_20'] = ta.trend.sma_indicator(df['Volume'], window=20) #20 day Volume SMA
 
+    # Our Indicators
+    df['DRI'] = df['Close']/df['SMA_20']
+    df['VAS'] = df['Volume']/df['Vol_20']
     return df
   
 df = pd.read_csv('ohlcv_1d.csv')
